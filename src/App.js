ef1094b4b8027b2679865aada4ce800b24a1dea7
erneston666 <erneston666@gmail.com>
2022-12-20 10:41:54 -0500

    formulario con useREF

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-function App() {
-  const submit = (e) =>{
-    e.preventDefault()
-    const Data = Array.from(new FormData(e.target))
-    console.log(Object.fromEntries(Data))
-  }
-  return (
-    <form onSubmit={submit}>
-      <div>
-        <span>
-          lala
-        </span>
-        <input name='campo' />
-      </div>
-      <input name='campo2' />
-      <input type='submit' value='Enviar' />
-    </form>
-  
-  );
+import { useRef } from "react"
+
+const App = () =>{
+    const input = useRef()
+    const file = useRef()
+    const submit = () =>{
+        console.log(input.current.value)
+        console.log(file.current.files[0])
+        const form = new FormData()
+        form.append('archivo',file.current.files[0])
+        form.append('campo',input.current.value)
+        fetch('/lala',{method: 'POST', body:form})
+    }
+    return(
+        <div>
+            <div>
+                <span>lala</span>
+                <input type='text' name ='campo' ref={input} />
+                <input type='file'  ref={file} />
+                
+            </div>
+            <input type='submit' value ='enviar' onClick={submit}/>
+        </div>
+    )
 }
 
-export default App;
+export default App
