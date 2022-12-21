4ea3fc5a3cdf4ac35a3ce8ba39624220b0ead1c8
erneston666 <erneston666@gmail.com>
2022-12-21 01:51:43 -0500

    controlando multiples inputs con useState

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import { useState } from "react"
 
 const App = () => {
-    const [value,setValue] = useState('')
+    const [value,setValue] = useState({normal:'por defecto' , texto: '' })
     const handleChange = (e) =>{
-        setValue(e.target.value)
+        console.log(e.target.name)
+       setValue((state) => ({
+        ...state,
+        [e.target.name]:e.target.value
+       }))
     }
     console.log(value)
     return (
         <div>
             {value.length < 5 ? <span>longitud minima de 5</span> : null}
-            <input name='normal' type='text' value={value} onChange={handleChange} />
+            <input name='normal' type='text' value={value.normal} onChange={handleChange} />
+            <textarea name='texto' value={value.texto}  onChange={handleChange}/>
         </div>
     )
 }
